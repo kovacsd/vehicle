436089d831092521ccbe332860c257eaaea7675b
kovacsd <kovacsdani014@gmail.com>
2024-03-17 13:42:42 +0100

    init

---
+++ sql_scripts/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE vehicles (
+    uuid VARCHAR(255) PRIMARY KEY,
+    rendszam VARCHAR(255),
+    tulajdonos VARCHAR(255),
+    forgalmi_ervenyes VARCHAR(255),
+    adatok VARCHAR(255)
+);
